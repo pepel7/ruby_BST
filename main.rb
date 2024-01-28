cf395ba66256c86518a4a8102d6457e3ed7d623f
pepel7 <dasha.per.2016@gmail.com>
2024-01-28 11:15:41 +0200

    Write driver script

--- main.rb
+++ main.rb
@@ -1,1 +1,27 @@
 require_relative './lib/tree'
+
+arr = (Array.new(15) { rand(1..100) }).uniq.sort
+tree = Tree.new(arr)
+tree.build_tree(arr, 0, arr.length - 1)
+puts "It's #{tree.balanced?} that the tree is proportional."
+puts "All elements in level order: #{tree.level_order}"
+puts "All elements in preorder: #{tree.preorder}"
+puts "All elements in postorder: #{tree.postorder}"
+puts "All elements in inorder: #{tree.inorder}"
+
+tree.pretty_print
+
+puts "\nUnbalancing the tree . . .\n\n"
+5.times { tree.insert(rand(101..200)) }
+puts "It's #{tree.balanced?} that the tree is proportional."
+
+puts "\nRebalancing the tree . . .\n\n"
+
+tree.rebalance
+puts "It's #{tree.balanced?} that the tree is proportional."
+puts "All elements in level order: #{tree.level_order}"
+puts "All elements in preorder: #{tree.preorder}"
+puts "All elements in postorder: #{tree.postorder}"
+puts "All elements in inorder: #{tree.inorder}"
+
+tree.pretty_print
